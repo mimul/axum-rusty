ef2f1be2bbe6d88586625b67d061aeec239fa64b
mimul <mimul@fittobe.com>
2025-02-05 23:39:17 +0900

    코드 리팩토링

--- domain/src/transaction.rs
+++ domain/src/transaction.rs
@@ -1,7 +1,6 @@
 use sqlx::{Acquire, Postgres};
-// https://qiita.com/FuJino/items/08b4c3298918191eab65
+
 pub trait PostgresAcquire<'c>: Acquire<'c, Database = Postgres> + Send {}
 impl<'c, T> PostgresAcquire<'c> for T
 where
-    T: Acquire<'c, Database = Postgres> + Send,
-{}
+    T: Acquire<'c, Database = Postgres> + Send {}
