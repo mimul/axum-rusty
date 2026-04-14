ef768d06eba847e2d630825a9ae336d53bfdeea8
mimul <mimul@fittobe.com>
2026-04-15 00:05:17 +0900

    refactor(infra): [R-R-10] todo SELECT SQL const로 중복 제거

--- infra/src/repository/todo.rs
+++ infra/src/repository/todo.rs
@@ -34,18 +34,19 @@ pub struct TodoRepository {
     db: Arc<dyn IDatabasePool>,
 }
 
+const SELECT_TODO_BY_ID: &str = r#"
+    SELECT t.id, t.title, t.description,
+           ts.id AS status_id, ts.code AS status_code, ts.name AS status_name,
+           t.created_at, t.updated_at
+    FROM todos t
+    INNER JOIN todo_statuses ts ON ts.id = t.status_id
+    WHERE t.id = $1
+"#;
+
 #[async_trait]
 impl ITodoRepository for TodoRepository {
     async fn get(&self, id: &Id<Todo>) -> anyhow::Result<Option<Todo>> {
-        let sql = r#"
-            SELECT t.id, t.title, t.description,
-                   ts.id AS status_id, ts.code AS status_code, ts.name AS status_name,
-                   t.created_at, t.updated_at
-            FROM todos t
-            INNER JOIN todo_statuses ts ON ts.id = t.status_id
-            WHERE t.id = $1
-        "#;
-        let result = query_as::<_, StoredTodo>(sql)
+        let result = query_as::<_, StoredTodo>(SELECT_TODO_BY_ID)
             .bind(id.value.to_string())
             .fetch_optional(self.db.pool())
             .await?;
@@ -93,15 +94,7 @@ impl ITodoRepository for TodoRepository {
     }
 
     async fn get_tx(&self, tx: &mut PgTx, id: &Id<Todo>) -> anyhow::Result<Option<Todo>> {
-        let sql = r#"
-            SELECT t.id, t.title, t.description,
-                   ts.id AS status_id, ts.code AS status_code, ts.name AS status_name,
-                   t.created_at, t.updated_at
-            FROM todos t
-            INNER JOIN todo_statuses ts ON ts.id = t.status_id
-            WHERE t.id = $1
-        "#;
-        let result = query_as::<_, StoredTodo>(sql)
+        let result = query_as::<_, StoredTodo>(SELECT_TODO_BY_ID)
             .bind(id.value.to_string())
             .fetch_optional(&mut **tx)
             .await?;
@@ -161,15 +154,7 @@ impl ITodoRepository for TodoRepository {
             .execute(&mut **tx)
             .await?;
 
-        let sql = r#"
-            SELECT t.id, t.title, t.description,
-                   ts.id AS status_id, ts.code AS status_code, ts.name AS status_name,
-                   t.created_at, t.updated_at
-            FROM todos t
-            INNER JOIN todo_statuses ts ON ts.id = t.status_id
-            WHERE t.id = $1
-        "#;
-        let stored = query_as::<_, StoredTodo>(sql)
+        let stored = query_as::<_, StoredTodo>(SELECT_TODO_BY_ID)
             .bind(id)
             .fetch_one(&mut **tx)
             .await?;
@@ -197,15 +182,7 @@ impl ITodoRepository for TodoRepository {
             .execute(&mut **tx)
             .await?;
 
-        let sql = r#"
-            SELECT t.id, t.title, t.description,
-                   ts.id AS status_id, ts.code AS status_code, ts.name AS status_name,
-                   t.created_at, t.updated_at
-            FROM todos t
-            INNER JOIN todo_statuses ts ON ts.id = t.status_id
-            WHERE t.id = $1
-        "#;
-        let stored = query_as::<_, StoredTodo>(sql)
+        let stored = query_as::<_, StoredTodo>(SELECT_TODO_BY_ID)
             .bind(id)
             .fetch_one(&mut **tx)
             .await?;
@@ -230,15 +207,7 @@ impl ITodoRepository for TodoRepository {
             .await
             .context(format!(r#"failed to upsert "{}" into todos"#, todo.id))?;
 
-        let sql = r#"
-            SELECT t.id, t.title, t.description,
-                   ts.id AS status_id, ts.code AS status_code, ts.name AS status_name,
-                   t.created_at, t.updated_at
-            FROM todos t
-            INNER JOIN todo_statuses ts ON ts.id = t.status_id
-            WHERE t.id = $1
-        "#;
-        let stored = query_as::<_, StoredTodo>(sql)
+        let stored = query_as::<_, StoredTodo>(SELECT_TODO_BY_ID)
             .bind(id)
             .fetch_one(&mut **tx)
             .await?;
